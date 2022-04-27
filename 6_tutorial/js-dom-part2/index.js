65c27774f763a54568eb048b91688c0603893146
Ekaterina Fedotova <fedotova490@gmail.com>
2022-04-27 18:26:53 +0200

    Add event listeners and removeCards

--- 6_tutorial/js-dom-part2/index.js
+++ 6_tutorial/js-dom-part2/index.js
@@ -4,7 +4,12 @@ const cardsContainer = document.getElementById('cards-container')
 // e.g. for class
 // document.querySelector('your-class')
 
+const addButton = document.getElementById('add-btn')
+
+const removeButton = document.getElementById('remove-btn')
+
 const createCards = () => {
+    removeCards()
     for (let i = 1; i < 6; i++) {
         // create element 
         const card = document.createElement('div')
@@ -17,4 +22,19 @@ const createCards = () => {
     }
 }
 
-createCards()
+const removeCards = () => {
+    // check if cardsContainer has child elements 
+    if (cardsContainer.hasChildNodes()){
+        //remove child elements 
+        while (cardsContainer.firstChild){
+            cardsContainer.removeChild(cardsContainer.firstChild)
+        }
+    }
+}
+
+
+// addEventListener - attaches an event handler to an element
+// element.addEventListener(event, function)
+addButton.addEventListener('click', createCards)
+removeButton.addEventListener('click', removeCards)
+

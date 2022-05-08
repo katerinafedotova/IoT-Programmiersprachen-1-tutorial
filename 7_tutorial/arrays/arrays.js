7dd3b118e6a35d5e5034f7bfbcd809872f6f488f
Ekaterina Fedotova <fedotova490@gmail.com>
2022-05-08 11:53:27 +0200

    Add arrays template for tutorial 7

---
+++ 7_tutorial/arrays/arrays.js
@@ -0,0 +1,22 @@
+// ARRAY - used to store a list of data; you can access the values by referring to an index number.
+
+// BASIC ARRAY METHODS 
+// see more methods here:
+// https://www.w3schools.com/js/js_array_methods.asp
+// https://www.w3schools.com/js/js_array_iteration.asp
+
+//ARRAY.PUSH() - adds one or more elements to the end of an array and returns the new length of the array.
+
+// ARRAY.LENGTH - returns the length of an array.
+
+// ARRAY.POP() - removes the last element of an array.
+
+// ARRAY.REVERSE() - reverses the order of the elements in an array.
+
+// ARRAY.SORT() - sorts the elements, IMPORTANT: it sorts them as strings in alphabetical and ascending order.
+// example with strings
+// example with numbers
+
+// ARRAY.forEach() - runs the provided function for each element in the array
+
+// ARRAY.JOIN() - The join() method creates and returns a new string by concatenating all of the elements in an array, separated by commas or a specified separator string.‚

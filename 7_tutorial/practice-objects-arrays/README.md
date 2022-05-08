a9f8e1d7705985ef23d60e094b4bb742ad1c4b21
Ekaterina Fedotova <fedotova490@gmail.com>
2022-05-08 12:49:23 +0200

    Add the task to practice during the lesson

---
+++ 7_tutorial/practice-objects-arrays/README.md
@@ -0,0 +1,17 @@
+# Practice arrays, objects and functions
+
+## Preparation
+1. Make sure you have ```Node.js``` installed. Check it by running ```node -v``` in your VS Code terminal: this command will return the version of Node.js you have. 
+If you have <b>no Node.js installed</b>, please download it here: https://nodejs.org/en/download/
+
+2. Copy this folder to your local PC.
+
+## Time
+40 minutes
+
+## Task description
+In this task you are going to create arrays && objects, interact with them using different built-in methods, as well as create and call functions. 
+
+Please follow the comments in the ```index.js``` and write needed functions. Whenever you see acomment with the ```console.log``` command, like ```console.log('initial todos ', todos)```, just uncomment this line. 
+
+After each uncommented ```console.log```, run ```node index.js``` in your VS Code terminal to see the outcome of your code.
---
+++ 7_tutorial/practice-objects-arrays/index.js
@@ -0,0 +1,35 @@
+// 1) Imitate a to-do list: create an array of 3 objects
+// each object should have 3 fields: "task" (a string (e.g., "Buy bread")), "person"(a string (e.g., "David")), "done" (boolean (true or false))
+
+// console.log('initial todos ', todos)
+
+// 2) Create a function "addToDo" that accepts a new todo object and adds it to our todos array
+
+// 3) Call this function with a new toDo object 
+
+// console.log('todos after add ', todos)
+
+// 4) Create a function "addId": 
+// inside this function loop through todos array using "Array.forEach" function
+// add a new field: "id" to each object and assign to it the index
+
+// 5) Call this function
+
+// console.log('todos with index ', todos)
+
+// 6) Reverse the array
+
+// console.log('array reversed ', todos)
+
+// 7) Remove the last element from the array
+
+// console.log('last elem removed ', todos)
+
+// 8) Console.log the length of your todos array
+
+// 9) Create a function "deletePersonField": use "for loop" and delete the field "person" from all the objects
+// see an example of deleting object field in the 6_tutorial -> objects-arrays
+
+// 10) Call this function
+
+// console.log('delete person ', todos)
